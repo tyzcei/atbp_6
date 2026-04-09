a71287b2c700b741a20180726c2c6d1e8e65b38a
tyzcei <a0181490@gmail.com>
2026-04-09 22:21:20 +0300

    Исправление ошибки, возврат к стабильной версии

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+  // Намеренная ошибка: возвращаем статус 500 и другой текст
+  res.status(500).send('Критическая ошибка сервера');
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
